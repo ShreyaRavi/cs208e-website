f620e94ad9bcef5cc9dd43cf62b8af7e8b2c4dab
aaru01 <94330380+aaru01@users.noreply.github.com>
2021-12-05 21:40:04 -0800

    Update Discussion.js

--- src/Discussion.js
+++ src/Discussion.js
@@ -7,7 +7,19 @@ class Discussion extends Component{
 
         return (
             <div>
-                <p> Discussion surrounding OOP </p>
+                <h1> Discourse surrounding Object Oriented Progamming </h1>
+                <h2>Advantages </h2>
+                <p> Object oriented programming has several high level advantages that can make the development process significantly more efficient. 
+                    As object-oriented programming is modular, as it provides separation of duties in object-based program development. 
+                    Objects can also be reused within an across applications - object-oriented programming languages come with rich libraries of objects, 
+                    and code developed during projects is also reusable in future projects. Because of its modularity and reusability object-oriented programming 
+                    provides improved software-development productivity (and as a result faster development) over traditional procedure-based programming techniques. </p>
+
+                <p> It is also easier to maintain since the design is modular, part of the system can be updated in case of issues without a need to make large-scale changes. 
+                    The reuse of software has the additional benefit of lowering the cost of development. Typically, more effort is put into the object-oriented 
+                    analysis and design, which lowers the overall cost of development. <\p>
+
+                <p> The outcome of all of this being higher-quality software, with faster development and lower cost allows allowing more time to be spent on testing.<\p>
             </div>
         );
     }
